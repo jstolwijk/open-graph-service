9280b22792f24ef5f134c4204bcd6618fab9c37a
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-04-05 00:53:50 +0200

    Use opengraph lib to parse open graph data from html

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/jstolwijk/open-graph-service
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/dyatlov/go-opengraph v0.0.0-20210112100619-dae8665a5b09
+	github.com/gin-gonic/gin v1.6.3
+	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,58 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/dyatlov/go-opengraph/opengraph"
+	"github.com/gin-gonic/gin"
+)
+
+type Query struct {
+	Url string `form:"url"`
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.GET("/open-graph", func(c *gin.Context) {
+		var query Query
+		if c.BindQuery(&query) != nil {
+			c.JSON(400, gin.H{
+				"code":    "QUERY_PARAM_MISSING",
+				"message": "'url' is a required query param",
+			})
+			return
+		}
+		resp, err := http.Get(query.Url)
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"code":                "INTERNA",
+				"origionalStatusCode": "asd",
+				"message":             err,
+			})
+			return
+		}
+		defer resp.Body.Close()
+		// body, err := ioutil.ReadAll(resp.Body)
+
+		// if err != nil {
+		// 	c.JSON(500, gin.H{
+		// 		"code":                "INTERNA",
+		// 		"origionalStatusCode": "asd",
+		// 		"message":             err,
+		// 	})
+		// 	return
+		// }
+
+		og := opengraph.NewOpenGraph()
+
+		err = og.ProcessHTML(resp.Body)
+
+		json, err := og.ToJSON()
+
+		c.Writer.Header().Add("Content-Type", "application/json")
+		c.Writer.Write(json)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
